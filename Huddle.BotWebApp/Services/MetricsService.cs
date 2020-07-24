e4618ad9f1b52f88d168563a73cb22d180b9af58
Tyler Lu <admin@tyler.lu>
2020-07-24 11:33:07 +0800

    Fix bug

--- Huddle.BotWebApp/Services/MetricsService.cs
+++ Huddle.BotWebApp/Services/MetricsService.cs
@@ -4,6 +4,8 @@
  */
 
 using Huddle.BotWebApp.Models;
+using Microsoft.Graph;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,26 +13,25 @@ namespace Huddle.BotWebApp.Services
 {
     public class MetricsService : GraphService
     {
-        private string baseSPSiteUrl;
+        private ISiteRequestBuilder _site;
 
         public MetricsService(string token, string baseSPSiteUrl) :
             base(token)
         {
-            this.baseSPSiteUrl = baseSPSiteUrl;
+            var url = new Uri(baseSPSiteUrl);
+            _site = _graphServiceClient.Sites.GetByPath(url.PathAndQuery.Trim('/'), url.Host);
         }
 
         public async Task<Metric[]> GetActiveMetricsAsync(string teamId)
         {
-            var site = this._graphServiceClient.Sites.Root.SiteWithPath(baseSPSiteUrl);
-
-            var issues = await site.Lists["Issues"].Items.Request()
+            var issues = await _site.Lists["Issues"].Items.Request()
                 .Select("Id")
                 .Filter($"fields/HuddleTeamId eq '{teamId}'")
                 .GetAllAsync();
+            if (issues.Length == 0) return new Metric[0];
 
             var fitler = string.Join(" or ", issues.Select(i => "fields/HuddleIssueLookupId eq " + i.Id));
-
-            var metrics = await site.Lists["Metrics"].Items.Request()
+            var metrics = await _site.Lists["Metrics"].Items.Request()
                 .Expand("fields($select=Title)")
                 .Filter(fitler)
                 .GetAllAsync();
